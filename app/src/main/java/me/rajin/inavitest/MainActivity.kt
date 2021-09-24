c7ea70a84702e704b61310410da05ddd0f85a67e
rajin <rajin9601@gmail.com>
2021-09-24 18:14:44 +0900

    remove viewBinding code

--- app/src/main/java/me/rajin/inavitest/MainActivity.kt
+++ app/src/main/java/me/rajin/inavitest/MainActivity.kt
@@ -2,13 +2,10 @@ package me.rajin.inavitest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import me.rajin.inavitest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.mapLayer
+        setContentView(R.layout.activity_main)
     }
 }
