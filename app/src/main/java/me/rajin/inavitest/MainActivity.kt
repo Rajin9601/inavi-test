4c1c3f307324b6cadb0330d076c6f56e2be304f3
rajin <rajin9601@gmail.com>
2021-09-24 16:40:53 +0900

    install inavi with viewBinding

--- app/src/main/java/me/rajin/inavitest/MainActivity.kt
+++ app/src/main/java/me/rajin/inavitest/MainActivity.kt
@@ -1,11 +1,14 @@
 package me.rajin.inavitest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.rajin.inavitest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mapLayer
     }
 }
